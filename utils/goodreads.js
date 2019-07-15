7fb3cba34fbf066a853cc22e69bcf251aa0a78c8
natalie.clamp <natalie.clamp@engineering.digital.dwp.gov.uk>
2019-07-15 15:16:03 +0100

    Configuring goodreads api call

---
+++ utils/goodreads.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+const convert = require('xml2js');
+
+const goodreadsURLBuilder = (title) => {
+    let encodedTitle = title.split(' ').join('+')
+    return `https://www.goodreads.com/book/title.xml?key=${process.env.GOODREADS_API_KEY}&title=${encodedTitle}`
+}
+
+// console.log(goodreadsURLBuilder('to kill a mockingbird'))
+
+const goodreads = {
+    getData: (title, cb) => {
+        axios.get(goodreadsURLBuilder(title))
+        .then(response => {
+            return convert.parseString(response.data, (err, result) => {
+                if (err) return console.log(err);
+                return result.GoodreadsResponse.book
+            })
+        })
+        .catch(err => console.log(err))
+    }
+}
+
+module.exports = goodreads;
