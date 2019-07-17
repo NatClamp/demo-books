1e8d8a58c0f3e7d6e1a1388e2d1b5100023e2a01
Natalie Clamp <nat.clamp@gmail.com>
2019-07-17 13:43:37 +0100

    Adding some test data to the database

--- db/data/books.js
+++ db/data/books.js
@@ -1,5 +1,17 @@
 module.exports = [
     {
-        "title": ""
-    }
+        "title": "'To Kill a Mockingbird (To Kill a Mockingbird, #1)",
+        "description": "The unforgettable novel of a childhood in a sleepy Southern town and the crisis of conscience that rocked it, To Kill A Mockingbird became both an instant bestseller and a critical success when it was first published in 1960. It went on to win the Pulitzer Prize in 1961 and was later made into an Academy Award-winning film, also a classic. Compassionate, dramatic, and deeply moving, To Kill A Mockingbird takes readers to the roots of human behavior - to innocence and experience, kindness and cruelty, love and hatred, humor and pathos. Now with over 18 million copies in print and translated into forty languages, this regional story by a young Alabama woman claims universal appeal. Harper Lee always considered her book to be a simple love story. Today it is regarded as a masterpiece of American literature.",
+        "author": "Harper Lee",
+    },
+    {
+        "title": "Ready Player One (Ready Player One, #1)",
+        "description": "IN THE YEAR 2044, reality is an ugly place. The only time teenage Wade Watts really feels alive is when he's jacked into the virtual utopia known as the OASIS. Wade's devoted his life to studying the puzzles hidden within this world's digital confines, puzzles that are based on their creator's obsession with the pop culture of decades past and that promise massive power and fortune to whoever can unlock them.  But when Wade stumbles upon the first clue, he finds himself beset by players willing to kill to take this ultimate prize. The race is on, and if Wade's going to survive, he'll have to win—and confront the real world he's always been so desperate to escape.",
+        "author": "Ernest Cline",
+    },
+    {
+        "title": "Outliers: The Story of Success",
+        "description": "In this stunning new book, Malcolm Gladwell takes us on an intellectual journey through the world of 'outliers'--the best and the brightest, the most famous and the most successful. He asks the question: what makes high-achievers different? His answer is that we pay too much attention to what successful people are like, and too little attention to where they are from: that is, their culture, their family, their generation, and the idiosyncratic experiences of their upbringing. Along the way he explains the secrets of software billionaires, what it takes to be a great soccer player, why Asians are good at math, and what made the Beatles the greatest rock band.",
+        "author": "Malcolm Gladwell",
+    },
 ]
