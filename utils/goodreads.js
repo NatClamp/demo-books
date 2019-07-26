1a6d26f9f91af83369a7c584d18590f32e52b93b
Natalie Clamp <nat.clamp@gmail.com>
2019-07-26 15:55:45 +0100

    refactor

--- utils/goodreads.js
+++ utils/goodreads.js
@@ -6,24 +6,19 @@ const goodreadsURLBuilder = (title) => {
     return `https://www.goodreads.com/book/title.xml?key=${process.env.GOODREADS_API_KEY}&title=${encodedTitle}`
 }
 
-const goodreads = {
-    getData: (title, cb) => {
-        axios.get(goodreadsURLBuilder(title))
-        .then(response => {
-            return convert.parseString(response.data, (err, result) => {
-                if (err) return console.log(err);
-                // console.log(result.GoodreadsResponse.book[0])
-                return result.GoodreadsResponse.book
-            })
+const getGoodreadsData = (title) => {
+    return axios.get(goodreadsURLBuilder(title))
+    .then(response => {
+        return convert.parseString(response.data, (err, result) => {
+            if (err) return console.log(err);
+            return result.GoodreadsResponse.book
         })
-        .catch(err => console.log(err))
-    }
+    })
+    .catch(err => console.log(err))
 }
-
-console.log(goodreads.getData('To kill a mockingbird'))
 
 // to get to the author field:
 // console.log(result.GoodreadsResponse.book[0].authors[0].author[0].name[0])
 
 
-module.exports = goodreads;
+module.exports = { getGoodreadsData };
