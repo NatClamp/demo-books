8fd2303708a0193dccb3876995e74dc4a0856c15
NatClamp <nat.clamp@gmail.com>
2019-07-26 18:34:11 +0100

    trying to fix bug with goodreads api

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,29 +1,28 @@
-const { getLibrary, getBook, postBook } = require('../models/book')
-const {getGoodreadsData} = require('../utils/goodreads')
+const { getLibrary, getBook, postBook } = require('../models/book');
+const { getGoodreadsData } = require('../utils/goodreads');
 
-exports.getBooks = (req,res,next) => {
-    getLibrary().then(books => {
-        res.status(200).render('library.njk', { books })
-    })
-}
+exports.getBooks = (req, res, next) => {
+  getLibrary().then(books => {
+    res.status(200).render('library.njk', { books });
+  });
+};
 
-exports.displayBook = (req,res,next) => {
-    getBook(req.params.id).then(book => {
-        book = book[0]
-        res.status(200).render('bookInfo.njk', { book })
-    })
-}
+exports.displayBook = (req, res, next) => {
+  getBook(req.params.id).then(book => {
+    book = book[0];
+    res.status(200).render('bookInfo.njk', { book });
+  });
+};
 
-exports.addNew = (req,res,next) => {
-    res.render('addNew.njk')
-}
+exports.addNew = (req, res, next) => {
+  res.render('addNew.njk');
+};
 
-exports.postNew = (req,res,next) => {
-    const bookTitle = req.body['title']
-    getGoodreadsData(bookTitle, (err, data) => {
-        if (err) return console.log(err)
-        console.log(data)
-        res.render('bookInfo.njk', {data})
-    })
-    // add this data to the database using postBook
-}
+exports.postNew = (req, res, next) => {
+  const bookTitle = req.body['title'];
+  return getGoodreadsData(bookTitle).then(data => {
+    res.render('bookInfo.njk', { data });
+  });
+
+  // add this data to the database using postBook
+};
--- utils/goodreads.js
+++ utils/goodreads.js
@@ -1,24 +1,25 @@
 const axios = require('axios');
 const convert = require('xml2js');
 
-const goodreadsURLBuilder = (title) => {
-    let encodedTitle = title.split(' ').join('+')
-    return `https://www.goodreads.com/book/title.xml?key=${process.env.GOODREADS_API_KEY}&title=${encodedTitle}`
-}
+const goodreadsURLBuilder = title => {
+  let encodedTitle = title.split(' ').join('+');
+  return `https://www.goodreads.com/book/title.xml?key=${
+    process.env.GOODREADS_API_KEY
+  }&title=${encodedTitle}`;
+};
 
-const getGoodreadsData = (title) => {
-    return axios.get(goodreadsURLBuilder(title))
-    .then(response => {
-        return convert.parseString(response.data, (err, result) => {
-            if (err) return console.log(err);
-            return result.GoodreadsResponse.book
-        })
-    })
-    .catch(err => console.log(err))
-}
+const getGoodreadsData = async title => {
+  try {
+    const response = await axios.get(goodreadsURLBuilder(title));
+    const data = await convert.parseString(response.data);
+    return data;
+  } catch (err) {
+    return console.log(err);
+  }
+};
 
+getGoodreadsData('lord of the rings').then(reply => console.log(reply));
 // to get to the author field:
 // console.log(result.GoodreadsResponse.book[0].authors[0].author[0].name[0])
-
 
 module.exports = { getGoodreadsData };
